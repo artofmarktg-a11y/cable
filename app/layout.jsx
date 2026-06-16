0579b7659c25b49a444abdf83abd14953fb421db
artofmarktg-a11y <artofmarktg@gmail.com>
2026-06-16 14:34:33 +0300

    Add Callibri tracking script

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Кабельная продукция со склада и под заказ",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script src="https://cdn.callibri.ru/callibri.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
